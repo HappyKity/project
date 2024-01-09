ac79692c8c07f17ddc595dbc5a641d7c4e17a428
hwang <hssxman0@naver.com>
2024-01-09 16:04:57 +0900

    덧셈함수선언

--- Day7/008.py
+++ Day7/008.py
@@ -7,5 +7,11 @@ def 제곱(a):
 numbers = [1,5,7]
 
 for item in numbers :
-    제곱(item)
+    제곱(item)  
     
+#정수 2개를 인자로 받아 덧셈 후 출력하는 함수를 정의하고 호출하시오
+def 덧셈(a,b):
+    c = a+b
+    print(c)
+
+덧셈(9,10)
