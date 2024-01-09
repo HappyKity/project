a6387bf9ad0c81e2fac263e9c10d7dcef74af189
hwang <hssxman0@naver.com>
2024-01-09 16:07:18 +0900

    함수 타입 지정 방법

--- Day7/008.py
+++ Day7/008.py
@@ -1,17 +1,21 @@
 def 제곱(a):
     print(a*a)
 
+
 제곱(10)
 제곱(100)
 
-numbers = [1,5,7]
+numbers = [1, 5, 7]
 
-for item in numbers :
-    제곱(item)  
-    
-#정수 2개를 인자로 받아 덧셈 후 출력하는 함수를 정의하고 호출하시오
-def 덧셈(a,b):
+for item in numbers:
+    제곱(item)
+
+# 정수 2개를 인자로 받아 덧셈 후 출력하는 함수를 정의하고 호출하시오
+
+
+def 덧셈(a: int | float, b: int | float):
     c = a+b
     print(c)
 
-덧셈(9,10)
+
+덧셈(9.5, 10)
